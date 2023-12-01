7f5ef6e148d6f119112bed9127fe462f4faae88c
unknown <cervinkova.sonik@gmail.com>
2023-12-01 18:54:19 +0000

    Function to prompt user for password options updated

--- starter/script.js
+++ starter/script.js
@@ -90,7 +90,38 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-
+  //Length of password prompt, between 8 and 128 characters
+  var userInput = prompt("How many characters should you password have? Please enter a number between 8 and 128.")
+    //Validate if the user has entered any value. If not, alert the user
+    if(!userInput){
+      alert("No value was entered. Please enter a number between 8 and 128.")
+    }
+    //Validate for the chosen length
+    //If a number is less than 8, alert the user
+    else if(userInput < 8){
+      alert(userInput + " is less than 8. Please enter a number between 8 and 128.")
+    }
+    //If a number is more than 128, alert the user
+    else if(userInput > 128){
+      alert(userInput + " is greater than 128. Please enter a number between 8 and 128.")
+    }
+    //Validate if the user input a number. If not, alert the user
+    else if(isNaN(userInput)){
+      alert(userInput + " is not a number. Please enter a number between 8 and 128.")
+    }
+    //if number between 8 and 128 entered, use confirm methods, so the user chooses from character types
+    else{
+      var containLowerCase = confirm("Should your password contain a lowercase?");
+      var containUpperCase = confirm("Should your password contain an uppercase?");
+      var containNumber = confirm("Should your password contain a number?");
+      var containSpecialChar =  confirm("Should your password contain a special character?"); 
+    }
+    if(!containLowerCase && !containUpperCase && !containNumber && !containSpecialChar){
+        alert("Your password must contain at least one character type")
+    }
+    else{
+      alert("Thank you")
+    }
 }
 
 // Function for getting a random element from an array
